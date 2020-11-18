ab402825e8b50a488bc17f6d5174ecae5112f02f
Karan Gohil <karandilipsinhgohil@gmail.com>
2020-11-18 12:30:50 -0800

    scripts added in analysis folder

---
+++ analysis/scripts/project_functions.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def load_and_process(urlOrPath):
+    df = (
+        pd.read_csv(urlOrPath)
+        .dropna()
+        .reset_index(col_level = 1)
+        .drop(['index'], axis = 1)
+    )
+
+    return df
